4a02382bc4ebf4e5e0e03987a8bcba2249a405f3
gikirima <58497634+gikirima@users.noreply.github.com>
2019-12-12 23:22:41 +0900

    aaa
    aaa

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 
 //make sure that all document is loaded first before applying js
 $(document).ready(function(){
-
+//loader function
 	$(function(){
 		$("#loader").fadeOut(600, function(){
 			$("#loader").remove();
@@ -19,12 +19,11 @@ $("#local-info").toggle();
 //toggle animation class
 $("#local-info").toggleClass("fadeInLeft");
 $("body").toggleClass("bg-1 bg-2");
-
+//toggle texts
 $("#local-text").text($("#local-text").text() == '現地情報' ? 'ヒーロー情報' : '現地情報');
 
-
-
-
 });
+
+
 
 });
